5ab68df63d1a6b8d05f7d23c4b831553cf989e89
Paula Borges <100873574+paulagmborges@users.noreply.github.com>
2024-01-26 20:21:59 -0300

    feat-paula

---
+++ desafio-1/modulo01_exercicio10.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade: ")
+cidadeFerias = input("Digite a cidade que você vai viajar nas férias: ")
+
+
+mensagem = f"Olá meu nome é {nome}, prazer em te conhecer. Tenho {idade} anos. Sou do {cidade}. Vou viajar para {cidadeFerias} nas minhas férias."
+print(mensagem)
