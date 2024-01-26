e7295655ebb25e2ddd35bce881647d45450d649b
Yasmin Tavares <108444835+yasmintavar3s@users.noreply.github.com>
2024-01-25 21:03:19 -0300

    Delete desafio-1/Ex4.py

--- desafio-1/Ex4.py
+++
@@ -1,6 +0,0 @@
-km = int(input("Digite quantos km você rodou: "))
-l= int(input("Digite quantos litros de combustível você gastou: "))
-
-consumo= km/l
-
-print(f'O seu carro tem o consumo médio de {consumo:.2f} km/l.')
