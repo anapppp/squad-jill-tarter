be12c81c93fb70fdc50812afec4c1d8d47b35588
SarahAffoncom <sarah.affonco@live.com>
2024-01-31 14:01:16 -0300

    organizacao

---
+++ Desafio 2/BancodeDados.py
@@ -0,0 +1,33 @@
+# Crie uma tabela chamada "alunos" com os seguintes campos:id(inteiro),nome(texto),idade(inteiro)ecurso(texto).
+#2. Insira pelo menos 5 registros de alunos na tabela que você criou no exercício anterior.
+
+#3. Consultas Básicas 
+# Escreva consultas SQL para realizar as seguintes tarefas
+# a)Selecionar todos os registros da tabela "alunos".
+# b)Selecionar o nome e a idade dos alunos com mais de 20anos.
+# c)Selecionar os alunos do curso de "Engenharia" em ordem alfabética.
+# d)Contar o número total de alunos na tabela
+
+#4. Atualização e Remoção
+#a)Atualize a idade de um aluno específico na tabela.
+#b)Remova um aluno pelo seu ID.
+
+#5. Criar uma Tabela e Inserir Dados
+#.Crie uma tabela chamada "clientes" com os campos: id (chave primária), nome (texto), idade (inteiro) e saldo (float). Insira alguns registros de clientes na tabela.
+
+#6. Consultas e Funções Agregadas
+#Escreva consultas SQL para realizar as seguintes tarefas:
+# a) Selecione o nome e a idade dos clientes com idade superior a 30 anos.
+# b) Calcule o saldo médio dos clientes.
+# c) Encontre o cliente com o saldo máximo.
+# d) Conte quantos clientes têm saldo acima de 1000.
+
+#7. Atualização e Remoção com Condições
+# a) Atualize o saldo de um cliente específico.
+# b) Remova um cliente pelo seu ID.
+
+#8. Junção de Tabelas
+# Crie uma segunda tabela chamada "compras" com os campos: id (chave primária), cliente_id (chave estrangeira 
+# referenciando o id da tabela "clientes"), produto (texto) e valor (real). Insira algumas compras associadas 
+# a clientes existentes na tabela "clientes". Escreva uma consulta para exibir o nome do cliente, o produto e o
+# valor de cada compra.
