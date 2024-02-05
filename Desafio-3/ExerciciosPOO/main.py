2e8277b9ca645d93ef7400840576ccb421c3d55d
anapppp <anapaula.ksoares@gmail.com>
2024-02-05 16:59:06 -0300

    first commit desafio 3 - Exercicio POO

---
+++ Desafio-3/ExerciciosPOO/main.py
@@ -0,0 +1,17 @@
+"""
+Gerenciamento de Mercado:
+
+Vamos criar um sistema orientado a objetos para representar um
+sistema de mercado seguindo os requisitos fornecidos:
+
+1. Cada produto pode ter um ou mais fornecedores.
+2. O mercado controla apenas o nome, o telefone e o endereço de
+cada cliente.
+3. Cada produto tem um nome, uma lista de categorias às quais
+pertence e uma quantidade disponível em estoque.
+4. Quando um produto é comprado, sua quantidade disponível em
+estoque é reduzida.
+5. O mercado mantém um registro de todas as transações
+realizadas, incluindo detalhes como data da compra, cliente
+envolvido e quantidade de produtos comprados.
+"""
