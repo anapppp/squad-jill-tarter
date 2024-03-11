1f97e45659fb187d770b319d917cd5927417fe40
Sarah Affonco <sarah.affonco@live.com>
2024-03-11 08:54:02 -0300

    Add files via upload

---
+++ Desafio-5/Introdução_testes_python/test_exercise.py
@@ -0,0 +1,34 @@
+import unittest
+
+class TestStrToBool(unittest.TestCase):
+
+    def test_y_is_true(self):
+        result = str_to_bool('y')
+        self.assertTrue(result)
+
+    def test_yes_is_true(self):
+        result = str_to_bool('Yes')
+        self.assertTrue(result)
+
+    def test_invalid_input(self):
+        with self.assertRaises(AttributeError):
+            str_to_bool(1)
+
+
+def str_to_bool(value):
+    try:
+        value = value.lower()
+    except AttributeError:
+        raise AttributeError(f"{value} must be of type string")
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
+    
+
+        
+if __name__ == '__main__':
+    unittest.main()
